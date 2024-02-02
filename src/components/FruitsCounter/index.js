7ba6c2aba011775088fe789cb2e00b6fdb94d06f
kedar Gaikwad <kedarga1@gmail.com>
2024-02-02 08:47:54 +0000

    button error

---
+++ src/components/FruitsCounter/index.js
@@ -0,0 +1,62 @@
+// Write your code here
+import './index.css'
+
+import {Component} from 'react'
+
+class FruitsCounter extends Component {
+  state = {mangoesCount: 0, bananasCount: 0}
+
+  mangoCount = () => {
+    this.setSate(prevState => ({mangoesCount: prevState.mangoesCount + 1}))
+  }
+
+  bananaCount = () => {
+    this.setSate(prevState => ({bananasCount: prevState.bananasCount + 1}))
+  }
+
+  render() {
+    const {mangoesCount, bananasCount} = this.state
+    return (
+      <div className="container">
+        <div className="fruite-container">
+          <h1 className="title">
+            Bob ate <span className="span">{mangoesCount}</span> mangoes
+            <span className="span"> {bananasCount} </span> bananas
+          </h1>
+          <div className="fruite-crad-container">
+            <div className="fruite-crad">
+              <img
+                className="image"
+                src="https://assets.ccbp.in/frontend/react-js/mango-img.png"
+                alt="mango"
+              />
+              <button
+                type="button"
+                className="button-1"
+                onClick={this.mangoCount}
+              >
+                Eat Mango
+              </button>
+            </div>
+            <div className="fruite-crad">
+              <img
+                className="image"
+                src="https://assets.ccbp.in/frontend/react-js/banana-img.png"
+                alt="banana"
+              />
+              <button
+                type="button"
+                className="button-1"
+                onClick={this.bananaCount}
+              >
+                Eat Banana
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default FruitsCounter
